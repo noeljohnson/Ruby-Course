fe01d562266120e614d0c245ce6a5ca46f658af4
Noel Johnson <noel.johnson6213@gmail.com>
2022-06-28 08:04:22 +0530

    Continue chapter 1

---
+++ Chapter 01/guess_num.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+##GTG
+
+# A game to guess the number in a limited number of tries with some feeback given with each guess
+# To win this game all the time, one of the ways would be to think like a binary search
+
+puts "Weclome to \'Get My Number!\'"
+print "Whats your name? "
+
+input = gets
+
+puts "Welcome, #{input}!"
+
+p input;
+
+##TYJC
