78690cb15f62ae20b24ad05e92305551ceea1641
Noel Johnson <noel.johnson6213@gmail.com>
2022-07-02 06:18:29 +0530

    Finished Chapter 1 started Chapter 2

--- Chapter 01/guess_num.rb
+++ Chapter 01/guess_num.rb
@@ -19,24 +19,28 @@ guessed_it = false
 puts "I have a number from 1 to 100"
 puts "Can you guess it"
 
-puts "#{10 - num_guesses} guesses left"
-print "Make a guess "
-guess = gets.to_i
 
-if (target > guess)
-  puts "Guess is low"
-elsif(target < guess)
-  puts "Guess is high"
-else
-  puts "Good job!, #{name}"
-  puts "You have guessed #{target} in #{10 - num_guesses}"
-  guessed_it = true
+while (num_guesses < 10 && !(guessed_it))
+
+  puts "#{10 - num_guesses} guesses left"
+  print "Make a guess "
+  guess = gets.to_i
+
+  if (target > guess)
+    puts "Guess is low"
+  elsif(target < guess)
+    puts "Guess is high"
+  else
+    puts "Good job!, #{name}"
+    puts "You have guessed #{target} in #{num_guesses + 1} tries"
+    guessed_it = true
+  end
+
+  num_guesses += 1
+
 end
 
-num_guesses += 1
-
-
-if not(guessed_it)
+unless (guessed_it)
   puts "The number was #{target}, you have lost the game"
 end
 
---
+++ Chapter 02/virt_test_drive.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby 
+##GTG
+
+#A program to simulate a virtual test drive
+
+def accelerate
+
+  puts "Steeping up the gas"
+  puts "Speeding Up"
+
+end
+
+def sound_horn
+
+  puts "Pressing the horn button"
+  puts "Beep Beep!"
+  
+end
+
+def use_headlights(brightness)
+
+  puts "Turning on #{brightness} headlights"
+  puts "Watch out for the deer"
+  
+end
+
+sound_horn
+accelerate
+use_headlights("High Beam")
+
+##TYJC
