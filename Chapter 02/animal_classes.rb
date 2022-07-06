fce0c02693b4f5f268287321d60b477a90bb5a3c
Noel Johnson <noel.johnson6213@gmail.com>
2022-07-06 13:16:54 +0530

    Continue chapter 2

--- Chapter 02/animal_classes.rb
+++ Chapter 02/animal_classes.rb
@@ -4,43 +4,67 @@
 
 class Bird
 
+  attr_accessor :name, :age
+
   def talk
-    puts "Chirp, Chirp!";
+    puts "#{name} says Chirp, Chirp!";
   end
 
   def move(destination)
-    puts "Flying to #{destination}";
+    puts "#{name} is flying to #{destination}";
+  end
+
+  def report_age
+    puts "#{name} is #{age} years old"
   end
 
 end
 
 class Cat
 
+  attr_accessor :name, :age
+
   def talk
-    puts "Meow!";
+    puts "#{name} says meow!";
   end
 
   def move(destination)
-    puts "Running to the #{destination}";
+    puts "#{name} is running to the #{destination}";
+  end
+
+  def report_age
+    puts "#{name} is #{age} years old"
   end
 
 end
 
 class Dog
 
+  attr_accessor :name, :age
+
   def talk
-    puts "Bark!";
+    puts "#{name} says Bark!";
   end
 
   def move(destination)
-    puts "Running to the #{destination}";
+    puts "#{name} is running to the #{destination}";
+  end
+
+  def report_age
+    puts "#{name} is #{age} years old"
   end
 
 end
 
 bird = Bird.new
+bird.name = "Jacky"
+bird.age = 2
 dog = Dog.new
+dog.name = "Max"
+dog.age = 12
 cat  = Cat.new
+cat.name = "KinPin"
+cat.age = 8
 
 bird.move("trees")
 dog.talk
---
+++ Chapter 02/mage.rb
@@ -0,0 +1,12 @@
+## GTG
+
+class Mage
+  attr_accessor :name, :spell;
+
+  def enchantment(target)
+    puts "#{@name} casts #{@spell} on #{target.name}"
+  end
+
+end
+
+## TYJC
