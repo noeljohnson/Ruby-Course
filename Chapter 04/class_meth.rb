d8e1e77b4cb8e82ebc1b54c149c59c678d34d492
Noel Johnson <noel.johnson6213@gmail.com>
2022-07-23 07:45:58 +0530

    Continue chapter 4

---
+++ Chapter 04/class_meth.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+##GTG
+class SimpleClass
+
+  def self.init(name, age)
+    puts("name = #{name}")
+    puts("age = #{age}")
+  end
+  
+end
+
+SimpleClass.init("Joe", 99)
+
+##TYJC
